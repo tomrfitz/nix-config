feb8f5309aa695161ec05c84b39393f0192b1302
Thomas FitzGerald <tomrfitz@gmail.com>
2026-03-02 01:21:25 -0500

    neat nix wsl logo
    thanks amp

--- modules/shared/home/fastfetch.nix
+++ modules/shared/home/fastfetch.nix
@@ -9,10 +9,18 @@
     enable = true;
     settings = {
       "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
-      logo.padding = {
-        top = 2;
-        left = 1;
-        right = 2;
+      logo = {
+        padding = {
+          top = 2;
+          left = 1;
+          right = 2;
+        };
+      }
+      // lib.optionalAttrs (hostName == "trfwsl") {
+        type = "kitty-direct";
+        source = "${../../../config/fastfetch-nixos-wsl.svg}";
+        width = 24;
+        height = 12;
       };
       display.separator = "  ";
       modules = [
