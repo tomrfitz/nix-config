dbbd540a643914d1e33b4f46509d35ab29d3086f
Thomas FitzGerald <tomrfitz@gmail.com>
2026-03-02 01:34:53 -0500

    let them be a lot longer

--- modules/shared/home/shell.nix
+++ modules/shared/home/shell.nix
@@ -252,13 +252,12 @@
       directory = {
         home_symbol = "~";
         truncation_length = 8;
+        truncate_to_repo = false;
         truncation_symbol = ".../";
         read_only = " [ro]";
         use_os_path_sep = true;
         style = "blue";
         format = "[$path]($style)[$read_only]($read_only_style)";
-        repo_root_style = "bold blue";
-        repo_root_format = "[$before_root_path]($before_repo_root_style)[$repo_root]($repo_root_style)[$path]($style)[$read_only]($read_only_style)";
       };
 
       cmd_duration.format = "[$duration ](white)";
