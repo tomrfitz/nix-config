d38b57300c5f96696ef782f5482e38b110fc6c2e
Thomas FitzGerald <tomrfitz@gmail.com>
2026-03-19 18:09:47 -0400

    font: fallback fonts for cjk

--- modules/shared/home/fonts.nix
+++ modules/shared/home/fonts.nix
@@ -3,9 +3,19 @@
   fonts.fontconfig = {
     enable = true;
     defaultFonts = {
-      sansSerif = [ "Atkinson Hyperlegible Next" ];
-      serif = [ "Iosevka Etoile" ];
-      monospace = [ "Atkinson Hyperlegible Mono" ];
+      sansSerif = [
+        "Atkinson Hyperlegible Next"
+        "Pretendard"
+        "Noto Sans CJK SC"
+      ];
+      serif = [
+        "Iosevka Etoile"
+        "Noto Sans CJK SC"
+      ];
+      monospace = [
+        "Atkinson Hyperlegible Mono"
+        "Noto Sans Mono CJK SC"
+      ];
       emoji = [ "Noto Color Emoji" ];
     };
   };
