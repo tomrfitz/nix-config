8a6a5187090fd1ef93b029e84cc4c5cd3c57a1c6
Thomas FitzGerald <tomrfitz@gmail.com>
2026-03-19 00:35:26 -0400

    emacs: remove doom

--- config/doom/packages.el
+++
@@ -1,7 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; $DOOMDIR/packages.el
-
-(package! flexoki-themes
-  :recipe (:host nil
-           :repo "https://codeberg.org/crmsnbleyd/flexoki-emacs-theme"
-           :files ("*.el")))
